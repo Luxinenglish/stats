ad8a6bfd1bf1e7b9962babdccf899ad07a7fae9c
Lux_ <80638883+Luxinenglish@users.noreply.github.com>
2025-10-05 13:47:49 +0200

    fix server

--- public/tracker.js
+++ public/tracker.js
@@ -3,10 +3,25 @@
     const page = window.location.pathname;
     const referrer = document.referrer;
     const userAgent = navigator.userAgent;
+    const language = navigator.language || navigator.userLanguage;
+    const screenResolution = `${screen.width}x${screen.height}`;
+    const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
 
     fetch('https://stat.pixelserver.fr/api/track', {
         method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ site, page, referrer, userAgent })
+        headers: {
+            'Content-Type': 'application/json',
+            'X-Forwarded-For': '', // Le proxy ajoutera cette en-tête automatiquement
+            'X-Real-IP': '' // Le proxy ajoutera cette en-tête automatiquement
+        },
+        body: JSON.stringify({
+            site,
+            page,
+            referrer,
+            userAgent,
+            language,
+            screenResolution,
+            timezone
+        })
     }).catch(() => {});
 })();
